4d884e5004d78ab9f8529101af20128c460e0901
Unwarymold9171 <unwarymold9171@gmail.com>
2024-12-09 20:50:04 -0500

    Renamed Project

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 
 /// A Python module implemented in Rust.
 #[pymodule]
-fn pypki(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn py_cert_store(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(store_reader::get_win_cert, m)?)?;
     Ok(())
--- test/test.py
+++ test/test.py
@@ -1,6 +1,6 @@
-import pypki
+import py_cert_store
 
-# print(pypki.__all__)
+# print(py_cert_store.__all__)
 
-print(pypki.sum_as_string(20, 5)) # 25
-print(pypki.get_win_cert('Digital Signature'))
+print(py_cert_store.sum_as_string(20, 5)) # 25
+print(py_cert_store.get_win_cert('Digital Signature'))
