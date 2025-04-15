12dc5856d8583d3e1033f99b1302314746710e9a
Unwarymold9171 <unwarymold9171@gmail.com>
2025-04-15 19:01:39 -0400

    Add comprehensive license information to multiple files

--- py_cert_store/py_cert_store.pyi
+++ py_cert_store/py_cert_store.pyi
@@ -1,3 +1,17 @@
+# Copyright 2025 Niky H. (Unwarymold9171)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 def find_windows_cert_by_extension(store:str="My", extension_oid:int=None, extension_value:str=None) -> str: # TODO change the return type to a dictionary (or list of dictionarys)
     """
     Find a certificate in the Windows certificate store by its extension.
