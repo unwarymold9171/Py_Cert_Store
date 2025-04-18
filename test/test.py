dcd6d27f33300eac9ac68841db4eb0ec6a41173b
Unwarymold9171 <unwarymold9171@gmail.com>
2025-04-18 14:00:13 -0400

    Making the test python file more verbose internally for later testing

--- test/test.py
+++ test/test.py
@@ -7,4 +7,6 @@ import py_cert_store
 # TODO: extention_oid needs to be derived from x509.OID_KEY_USAGE
 # To handel this, I may need to add a hashmap matching the Cryptography OID to the Windows OIDs in the module
 
-print(py_cert_store.get_win_cert())
+certificate = py_cert_store.get_win_cert(return_as_dict=True, verbose=False)
+
+print(certificate)
