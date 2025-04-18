4737431abc7185347751d7800959e74a03de69f2
Unwarymold9171 <unwarymold9171@gmail.com>
2025-04-18 19:03:13 -0400

    Print Statements to the test script
    Prints out all the values to help see if there are any errors in the expected output
    Added a final printout that shows what parts passed vs failed the final expected output

--- test/test.py
+++ test/test.py
@@ -7,27 +7,43 @@ import py_cert_store
 # TODO: extention_oid needs to be derived from x509.OID_KEY_USAGE
 # To handel this, I may need to add a hashmap matching the Cryptography OID to the Windows OIDs in the module
 
-certificate = py_cert_store.get_win_cert(return_as_dict=True, verbose=False)
+rust_cert = py_cert_store.get_win_cert(return_as_dict=True, verbose=False)
 
-print(certificate)
-
+print(rust_cert)
 
 
 from wincert import WinCerts
 
 with WinCerts() as certs:
-    cert = certs.get_cert(return_as_dict=True, verbose=False)
+    dotnet_cert = certs.get_cert(return_as_dict=True, verbose=False)
 
 
-print("Rust FriendlyName: ", certificate["FriendlyName"])
-print(".NET FriendlyName: ", cert["FriendlyName"])
-print("Rust Name: ", certificate["Name"])
-print(".NET Name: ", cert["Name"])
-print("Rust EffectiveDateString: ", certificate["EffectiveDateString"])
-print(".NET EffectiveDateString: ", cert["EffectiveDateString"])
-print("Rust ExpirationDateString: ", certificate["ExpirationDateString"])
-print(".NET ExpirationDateString: ", cert["ExpirationDateString"])
-# print("Rust Certificate: ", certificate["cert"])
-# print(".NET Certificate: ", cert["cert"])
+print("Rust FriendlyName: ", rust_cert["FriendlyName"])
+print(".NET FriendlyName: ", dotnet_cert["FriendlyName"])
 
-print("Certificates are equal: ", certificate["cert"] == cert["cert"])
+print("Rust Name: ", rust_cert["Name"])
+print(".NET Name: ", dotnet_cert["Name"])
+
+print("Rust IssuerName: ", rust_cert["IssuerName"])
+print(".NET IssuerName: ", dotnet_cert["IssuerName"])
+
+print("Rust EffectiveDateString: ", rust_cert["EffectiveDateString"])
+print(".NET EffectiveDateString: ", dotnet_cert["EffectiveDateString"])
+
+print("Rust ExpirationDateString: ", rust_cert["ExpirationDateString"])
+print(".NET ExpirationDateString: ", dotnet_cert["ExpirationDateString"])
+
+
+print("Certificates are equal: ", rust_cert["cert"] == dotnet_cert["cert"])
+
+pass_fail = {
+    "FriendlyName": rust_cert["FriendlyName"] == dotnet_cert["FriendlyName"],
+    "Name": rust_cert["Name"] == dotnet_cert["Name"],
+    "IssuerName": rust_cert["IssuerName"] == dotnet_cert["IssuerName"],
+    "EffectiveDateString": rust_cert["EffectiveDateString"] == dotnet_cert["EffectiveDateString"],
+    "ExpirationDateString": rust_cert["ExpirationDateString"] == dotnet_cert["ExpirationDateString"],
+    "cert": rust_cert["cert"] == dotnet_cert["cert"],
+}
+
+print("Pass/Fail: ", pass_fail)
+print("All tests pass: ", all(pass_fail.values()))
